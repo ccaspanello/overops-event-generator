99204d6f9d1d6b3d864778a5b2fc5cb8a5db892b
Administrator <admin@example.com>
2020-04-29 21:04:05 +0000

    Update quality-report.sh

--- quality-report.sh
+++ quality-report.sh
@@ -7,7 +7,7 @@ echo "*************************"
 http_code=$(
   curl -v \
     --header "x-api-key: ${OVEROPS_API_KEY}" \
-    --data "query={applicationName: event-generator, serviceId: ${OVEROPS_ENVIRONMENT_ID}, deploymentName: deployment-${CI_JOB_ID}, markUnstable: true, newEvents: true, resurfacedErrors: true, maxErrorVolume: 10, maxUniqueErrors: 10, criticalExceptionTypes:'ExampleUncaughtException, SAXParseException', activeTimeSpan: '0m', baselineTimespan: '1d', minVolumeThreshold: 1, minErrorRateThreshold: 0.0001, regressionDelta: 0.0001, criticalRegressionDelta: 0.0001  }" \
+    --data "query={applicationName: ${CI_PROJECT_NAME}, serviceId: ${OVEROPS_ENVIRONMENT_ID}, deploymentName: job-${CI_COMMIT_SHORT_SHA}, markUnstable: true, newEvents: true, resurfacedErrors: true, maxErrorVolume: 10, maxUniqueErrors: 10, criticalExceptionTypes:'ExampleUncaughtException, SAXParseException', activeTimeSpan: '0m', baselineTimespan: '1d', minVolumeThreshold: 1, minErrorRateThreshold: 0.0001, regressionDelta: 0.0001, criticalRegressionDelta: 0.0001  }" \
     --request POST \
     --url http://localhost:8080/report/v1 \
     --output overops-quality-report/report.html \
