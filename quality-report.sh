da48170cb0ec2a9900c9dbc52d5b61b0ed1a7748
Administrator <admin@example.com>
2020-04-29 21:17:38 +0000

    Update quality-report.sh

--- quality-report.sh
+++ quality-report.sh
@@ -9,7 +9,7 @@ http_code=$(
     --header "x-api-key: ${OVEROPS_API_KEY}" \
     --data "query={applicationName: ${CI_PROJECT_NAME}, serviceId: ${OVEROPS_ENVIRONMENT_ID}, deploymentName: job-${CI_COMMIT_SHORT_SHA}, markUnstable: true, newEvents: true, resurfacedErrors: true, maxErrorVolume: 10, maxUniqueErrors: 10, criticalExceptionTypes:'ExampleUncaughtException, SAXParseException', activeTimeSpan: '0m', baselineTimespan: '1d', minVolumeThreshold: 1, minErrorRateThreshold: 0.0001, regressionDelta: 0.0001, criticalRegressionDelta: 0.0001  }" \
     --request POST \
-    --url http://localhost:8080/report/v1 \
+    --url ${OVEROPS_BACKEND_URL}/report/v1 \
     --output overops-quality-report/report.html \
     --write-out '%{http_code}'
 )
