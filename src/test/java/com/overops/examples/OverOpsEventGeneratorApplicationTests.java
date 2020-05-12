8aff3b0a4ac776ce23c8fe9ccf5ab04b475aef52
Chris Caspanello <ccaspanello@gmail.com>
2020-05-12 15:21:02 -0400

    Adding some logic to the test so it will generate errors when running

--- src/test/java/com/overops/examples/OverOpsEventGeneratorApplicationTests.java
+++ src/test/java/com/overops/examples/OverOpsEventGeneratorApplicationTests.java
@@ -1,7 +1,13 @@
 package com.overops.examples;
 
+import com.overops.examples.controller.Controller;
+import com.overops.examples.domain.User;
+import com.overops.examples.domain.UserRepository;
+import com.overops.examples.service.LoggedErrorService;
+import com.overops.examples.utils.EventType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -11,8 +17,22 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ActiveProfiles("test")
 public class OverOpsEventGeneratorApplicationTests {
 
+    @Autowired
+    private Controller controller;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
     public void contextLoads() {
+
+        int counter = 0;
+        for (int i = 0; i < 100; i++) {
+            for (User user : userRepository.findAll()) {
+                counter++;
+                controller.route(counter, user);
+            }
+        }
     }
 
 }
